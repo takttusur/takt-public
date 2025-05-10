6fae629b85a27c1d0669d2e84cd6ca20e6216819
andreygolubkow <andreygolubkow@gmail.com>
2025-05-10 12:44:49 +0400

    Remove Charkaui from home page

--- src/routes/HomePage.tsx
+++ src/routes/HomePage.tsx
@@ -1,25 +1,13 @@
-import { Box, Center, VStack, Image, Link, Text } from '@chakra-ui/react'
 import { Link as RouterLink } from 'react-router-dom'
 import taktLogo from '../assets/takt.svg'
 
 export default function HomePage(): JSX.Element {
     return (
-        <Box
-            height="100vh"
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-        >
-            <VStack spacing={6}>
-                <Center>
-                    <Image src={taktLogo} alt="TAKT Logo" boxSize="200px" />
-                </Center>
-                <Link as={RouterLink} to="/inmemoria">
-                    <Text fontSize="2xl" fontWeight="bold">
-                        InMemoria
-                    </Text>
-                </Link>
-            </VStack>
-        </Box>
+        <div>
+            <img src={taktLogo} alt="TAKT Logo" />
+            <RouterLink to="/inmemoria">
+                <span>InMemoria</span>
+            </RouterLink>
+        </div>
     )
 }
