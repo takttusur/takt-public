b191c28304352ea9a8e8fd6a6851851cedc83724
andreygolubkow <andreygolubkow@gmail.com>
2025-05-16 00:26:01 +0400

    Remove footer from whole app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css'
-import Footer from './components/Footer'
 
-import { useRoutes, useLocation } from 'react-router-dom'
+import { useRoutes } from 'react-router-dom'
 import rootRoutes from './routes/'
 import YandexMetrikaCounter from './components/Common/YandexMetrikaCounter.tsx'
 import EnvironmentService from './services/EnvironmentService'
@@ -10,15 +9,10 @@ const routes = rootRoutes.getRoutes()
 
 export default function App(): JSX.Element {
     const routesElement = useRoutes(routes)
-    const location = useLocation()
-
-    // Check if the current route is /inmemoria
-    const isInMemoriaRoute = location.pathname === '/inmemoria'
 
     return (
         <>
             {routesElement}
-            {!isInMemoriaRoute && <Footer />}
             <YandexMetrikaCounter
                 isEnabled={EnvironmentService.YandexMetrikaEnabled}
                 id={EnvironmentService.YandexMetrikaId}
